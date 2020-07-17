40aca5e828a7fb3036b31f89e5e5fa63b6dd7d32
Nikola Nikolovski <nikola.joysoi@gmail.com>
2020-07-16 20:51:09 -0400

    added city object in weather response object

--- app/src/main/java/com/example/weatherforecast/data/model/future/FutureWeatherResponse.kt
+++ app/src/main/java/com/example/weatherforecast/data/model/future/FutureWeatherResponse.kt
@@ -5,5 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class FutureWeatherResponse(
     @SerializedName("list")
-    val list: List<FutureWeatherEntry> = listOf()
+    val list: List<FutureWeatherEntry> = listOf(),
+    @Embedded(prefix = "city_")
+    val city: City = City()
 )
